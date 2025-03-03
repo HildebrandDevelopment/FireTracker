0cf23fb9c6f7017d17113150a96e3e974a63512a
HildebrandDevelopment <HildebrandDevelopment>
2025-03-03 09:10:45 -0600

    Updating FireTracker database creation scripts.

--- resources/Database/DBScripts/02_CreateLogin.sql
+++ resources/Database/DBScripts/02_CreateLogin.sql
@@ -3,7 +3,7 @@ GO
 
 /* For security reasons the login is created disabled and with a random password. */
 /****** Object:  Login [firetracker_app]    Script Date: 2/20/2025 7:16:54 PM ******/
-CREATE LOGIN [firetracker_app] WITH PASSWORD=N'iwj31msbLlwIoxqhnLGs2iwsJH+a8pPy8lrPQBltjzo=', DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=ON
+CREATE LOGIN [firetracker_app] WITH PASSWORD=N'Pwd123#sa_pwd#', DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=ON
 
 GO
 
--- resources/Database/DBScripts/03_AddUserToDB.sql
+++ resources/Database/DBScripts/03_AddUserToDB.sql
@@ -1,14 +1,20 @@
-USE [FireTrackerDb]
-GO
-CREATE USER [firetracker_app] FOR LOGIN [firetracker_app]
-GO
-USE [FireTrackerDb]
-GO
-ALTER ROLE [db_owner] ADD MEMBER [firetracker_app]
-GO
-
+/*
 USE [master]
 GO
 
 CREATE USER [firetracker_app] FOR LOGIN [firetracker_app] WITH DEFAULT_SCHEMA=[dbo]
 GO
+*/
+
+USE [FireTrackerDb]
+GO
+CREATE USER [firetracker_app] FOR LOGIN [firetracker_app]
+GO
+
+
+USE [FireTrackerDb]
+GO
+ALTER ROLE [db_owner] ADD MEMBER [firetracker_app]
+GO
+
+
